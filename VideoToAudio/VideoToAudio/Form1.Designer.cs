aa1bff865189f5550418844de1ead96ad3974d32
matthewb2 <bestsciencekwabe@gmail.com>
2023-06-26 22:22:34 +0900

    video to audio comvert

---
+++ VideoToAudio/VideoToAudio/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace VideoToAudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "열기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 68);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(514, 190);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(421, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "변환";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(441, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "닫기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 315);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ VideoToAudio/VideoToAudio/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoToAudio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            listView1.View = View.Details;
+
+            
+
+
+        }
+        private void ResizeListViewColumns(ListView lv)
+        {
+            foreach (ColumnHeader column in lv.Columns)
+            {
+                column.Width = -2;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = @"C:\Users\bests\Downloads";
+                
+                openFileDialog.Filter = "video files (*.mp4)|*.mp4|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.Multiselect = true;
+                openFileDialog.RestoreDirectory = true;
+
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+
+                    listView1.Columns.Add("제목");
+                    listView1.Columns.Add("크기");
+                    listView1.Columns.Add("날짜");
+
+                    
+                    foreach (String filepath in openFileDialog.FileNames)
+                    {
+                            //list_file.Items.Add(filepath.ToString());
+                            //string[] row = { videoName, "2", "3" };
+                            //var listViewItem = new ListViewItem(row);
+                            listView1.Items.Add(filepath);
+                            
+
+
+                    }
+
+
+
+                    //string audioName = Path.GetFileNameWithoutExtension(filepath) + ".mp3";
+
+                    string videoName = openFileDialog.FileName;
+                    
+                    
+                    ResizeListViewColumns(listView1);
+                    //
+                    // ffmpeg - i filename.mp4 filename.mp3
+                    /*
+                    string files = "-i " + "\"" + videoName + "\" " + audioName;
+                    
+                    string cmdtext = "/C ffmpeg " + files;
+                    System.Diagnostics.Process.Start("CMD.exe", cmdtext);
+                    */
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            foreach (var item in listView1.Items)
+            {
+                //Console.WriteLine(item.ToString());
+                string path = Path.GetDirectoryName(item.ToString());
+
+                path = path.Replace("ListViewItem: {", "");
+                //path = path.Replace("}", "");
+                string videoName = item.ToString().Replace("ListViewItem: {", "");
+                videoName = videoName.Replace("}", "");
+                string audioName = path + @"\"+Path.GetFileNameWithoutExtension(item.ToString()) + ".mp3";
+
+                //Console.WriteLine(videoName);
+                //Console.WriteLine(audioName);
+
+                string files = "-i " + "\"" + videoName + "\" " + "\"" + audioName+"\" ";
+                Console.WriteLine(files);
+                string cmdtext = "/C ffmpeg " + files;
+                System.Diagnostics.Process.Start("CMD.exe", cmdtext);
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+    }
+}
